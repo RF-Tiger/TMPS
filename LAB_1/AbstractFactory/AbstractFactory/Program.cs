b22d3ef2c28b3068a21aeeabb48f87f84489a806
CW_Kocegar <36210551+dimon3d@users.noreply.github.com>
2019-04-22 04:41:35 +0300

    Add files via upload

---
+++ LAB_1/AbstractFactory/AbstractFactory/Program.cs
@@ -0,0 +1,17 @@
+﻿using AbstractFactory.ConcreteClass;
+using System;
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = null;
+            client = new Client(new CocaColaFactory());
+            client.Run();
+            client = new Client(new PepsiFactory());
+            client.Run();
+        Console.ReadKey();
+        }
+    }
+}
---
+++ LAB_1/Singletone/Singletone/Singletone/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TheBell s1 = TheBell.Instance();
+            TheBell s2 = TheBell.Instance();
+            Console.WriteLine(s1.GetHashCode());
+            Console.WriteLine(s2.GetHashCode());
+
+            Console.ReadKey();
+        }
+    }
+}
